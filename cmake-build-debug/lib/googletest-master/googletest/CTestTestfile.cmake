600c28ed75ff9418132bf45ff0414f124ed33570
Tobi <tobipristupin@gmail.com>
2020-05-28 01:47:21 -0400

    Removed cmake build files

--- cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pristu/Documents/lox/lib/googletest-master/googletest
-# Build directory: /home/pristu/Documents/lox/cmake-build-debug/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
